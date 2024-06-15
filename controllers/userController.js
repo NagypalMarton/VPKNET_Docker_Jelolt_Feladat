8af96ded4d8dd21e06294060e4e18a19e67c4522
Nagypál Márton <you@example.com>
2024-06-15 20:38:33 +0200

    HTTP státuszkódot használok hibakezelésre

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,13 +8,15 @@ exports.postLogin = (req, res) => {
   const { neptun, password } = req.body;
   
   db.query('SELECT * FROM users WHERE neptun = ? AND password = ?', [neptun, password], (err, results) => {
-    if (err) throw err;
+    if (err) {
+      return res.status(500).send('Szerverhiba történt.');
+    }
     
     if (results.length > 0) {
       req.session.user = results[0];
       res.redirect('/');
     } else {
-      res.render('login', { error: 'Érvénytelen Neptun kód vagy jelszó' });
+      res.status(401).render('login', { error: 'Érvénytelen Neptun kód vagy jelszó' });
     }
   });
 };
@@ -29,7 +31,9 @@ exports.postRegister = (req, res) => {
   const { neptun, password, name, email } = req.body;
   
   db.query('INSERT INTO users (neptun, password, name, email) VALUES (?, ?, ?, ?)', [neptun, password, name, email], (err, results) => {
-    if (err) throw err;
+    if (err) {
+      return res.status(500).send('Szerverhiba történt a regisztráció során.');
+    }
     
     req.session.user = { id: results.insertId, neptun, name, email };
     res.redirect('/');
