851b8005643ac46500e5b62564497b519ec271d1
Nagypál Márton <you@example.com>
2024-06-16 20:12:57 +0200

    Updated website architecture. Dynamic body

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,27 @@
 // controllers/userController.js
 const connection = require('../models/userModel');
+const ejs = require('ejs');
 
 exports.getUsers = (req, res) => {
   connection.query('SELECT * FROM users', (err, results) => {
     if (err) throw err;
-    res.render('index', { users: results });
+    ejs.renderFile('views/index.ejs', { users: results }, (err, html) => {
+      if (err) throw err;
+      res.render('main', { 
+        title: 'User List',
+        body: html
+      });
+    });
+  });
+};
+
+// Assuming you have a controller function for the new user page
+exports.newUserForm = (req, res) => {
+  ejs.renderFile('views/newUser.ejs', {}, (err, html) => {
+    if (err) throw err;
+    res.render('main', { 
+      title: 'Add New User',
+      body: html 
+    });
   });
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,10 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
+// Existing route for the user list
 router.get('/', userController.getUsers);
+
+// Add a route for the new user form
+router.get('/users/new', userController.newUserForm);
 
 module.exports = router;
